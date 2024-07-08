186e545ea8b0fb4a309c0733e04d854ba8460f8f
Manik <tonysins43@gmail.com>
2024-07-08 13:06:09 +0600

    create table for authors

--- src/db/migrations/20240708061435_create_authors.ts
+++ src/db/migrations/20240708061435_create_authors.ts
@@ -1,5 +1,14 @@
-import type { Knex } from 'knex'
+import { Knex } from 'knex';
 
-export async function up(knex: Knex): Promise<void> {}
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('authors', (table) => {
+    table.increments('id');
+    table.string('name').notNullable();
+    table.text('bio').nullable();
+    table.date('birthdate').notNullable();
+  });
+}
 
-export async function down(knex: Knex): Promise<void> {}
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('authors');
+}
