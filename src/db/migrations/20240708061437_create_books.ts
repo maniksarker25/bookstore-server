2c0a1ea9c4c105c1f51fa2b2e0b8193e3f8287d0
Manik <tonysins43@gmail.com>
2024-07-08 13:13:44 +0600

    create schema for book table

--- src/db/migrations/20240708061437_create_books.ts
+++ src/db/migrations/20240708061437_create_books.ts
@@ -1,10 +1,12 @@
-import type { Knex } from "knex";
-
+import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('books', (table) => {
+    table.increments('id');
+    table.string('title').notNullable();
+    table.text('description').notNullable();
+    table.date('published_date').notNullable();
+    table.integer('author_id').unsigned().notNullable();
+    table.foreign('author_id').references('authors.id');
+  });
 }
-
-
-export async function down(knex: Knex): Promise<void> {
-}
-
